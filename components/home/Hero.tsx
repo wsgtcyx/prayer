9eff6d3067461bb517db2d2bfd92293f118dd538
wsgtcyx <439248005@qq.com>
2024-07-14 13:41:31 +0800

    update ui in ios

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -21,13 +21,14 @@ const Hero = () => {
         }}
       >
         <section className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-16 md:pt-24 text-center">
-          <h1>
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold tracking-tight">
             Create Personalized<LineText> Prayers</LineText> with AI
           </h1>
-          <p className="mx-auto mt-6 max-w-2xl text-2xl tracking-tight text-slate-700 dark:text-slate-400">
+          <p className="mx-auto mt-6 max-w-lg sm:max-w-xl lg:max-w-2xl text-lg sm:text-xl lg:text-2xl tracking-tight text-slate-700 dark:text-slate-400">
             Tailor your prayers for healing, peace, and strength in just one click. Let our AI guide you in crafting the customized prayer.
           </p>
         </section>
+
       </motion.div>
       <CTAButton />
     </>
