ed7e963703964c0db2e4b47abcce8cc1a03df759
wsgtcyx <439248005@qq.com>
2024-07-16 09:06:21 +0800

    update blog post page

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -2,13 +2,21 @@
 import PostList from "@/components/post/PostList";
 import { getPosts } from "@/lib/post";
 
+export async function generateMetadata() {
+  return {
+    title: "Prayer Articles - Find the Tips for Perfect Prayers ",
+    description: "Explore our extensive collection of prayer articles at PrayerAI. Find prayers for healing, peace, strength, and more.",
+    keywords: "prayer, prayers for healing, customized prayers, AI prayers, how to pray, best prayers, prayer articles",
+  };
+}
+
 export default async function PostPage() {
   const { posts } = await getPosts();
 
   return (
-    <div className="flex flex-row w-full pt-12">
-      <div className="hidden md:block md:w-1/5 pl-6"></div>
+    <div className="flex flex-col items-center w-full pt-12">
       <div className="w-full md:w-3/5 px-6">
+        <h1 className="text-4xl font-bold mb-8 text-center">Discover Powerful Prayers and Prayer Tips</h1>
         <PostList posts={posts} />
       </div>
     </div>
